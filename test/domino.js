fgnass/domino

commit 400b989d96d3d0b11482808ab0cccbda98ee3e98

Fix the SIMPLETAG and SIMPLEATTR special cases in the parse; add test cases.
The SIMPLETAG and SIMPLEATTR special-cases were broken (the regexps
would never match) when the code was rewritten not to use the sticky
tag (regex "y" flag) which is in Firefox and ES6 but not yet in other
JS engines.  Rewrite these regular expressions so that they don't
require the sticky flag.

Add a test case which exercises the SIMPLEATTR handling as well as testing
the \r issues fixed in the previous few patches.

lib/HTMLParser.js
@@ -1448,8 +1448,13 @@ var TAGNAME = /[^\r\t\n\f \/>A-Z\u0000]+/g;
 var DATATEXT = /[^&<\r\u0000\uffff]*/g;
 var RAWTEXT = /[^<\r\u0000\uffff]*/g;
 var PLAINTEXT = /[^\r\u0000\uffff]*/g;
-var SIMPLETAG = /^(\/)?([a-z]+)>/g;
-var SIMPLEATTR = /^([a-z]+) *= *('[^'&\r\u0000]*'|"[^"&\r\u0000]*"|[^&> \t\n\r\f\u0000][ \t\n\f])/g;
+// Since we don't have the 'sticky tag', add '|.' to the end of SIMPLETAG
+// and SIMPLEATTR so that we are guaranteed to always match.  This prevents
+// us from scanning past the lastIndex set. (Note that the desired matches
+// are always greater than 1 char long, so longest-match will ensure that .
+// is not matched unless the desired match fails.)
+var SIMPLETAG = /(?:(\/)?([a-z]+)>)|./g;
+var SIMPLEATTR = /(?:([-a-z]+) *= *('[^'&\r\u0000]*'|"[^"&\r\u0000]*"|[^&> \t\n\r\f\u0000]+[ \t\n\f]))|./g;
 
 var NONWS = /[^\x09\x0A\x0C\x0D\x20]/;
 var ALLNONWS = /[^\x09\x0A\x0C\x0D\x20]/g; // like above, with g flag
@@ -2261,8 +2266,9 @@ function HTMLParser(address, fragmentContext, options) {
   function handleSimpleAttribute() {
     SIMPLEATTR.lastIndex = nextchar-1;
     var matched = SIMPLEATTR.exec(chars);
-    if (!matched) return false;
+    if (!matched) throw new Error("should never happen");
     var name = matched[1];
+    if (!name) return false;
     var value = matched[2];
     var len = value.length;
     switch(value[0]) {
@@ -2399,8 +2405,9 @@ function HTMLParser(address, fragmentContext, options) {
   function emitSimpleTag() {
     SIMPLETAG.lastIndex = nextchar;
     var matched = SIMPLETAG.exec(chars);
-    if (!matched) return false;
+    if (!matched) throw new Error("should never happen");
     var tagname = matched[2];
+    if (!tagname) return false;
     var endtag = matched[1];
     if (endtag) {
       nextchar += (tagname.length+2);

test/domino.js
@@ -305,3 +305,12 @@ exports.attributeSelector = function() {
 
   h1.matches('*[isHTML]').should.equal(false);
 };
+
+exports.crHandling = function() {
+  var html = '<div\rid=a data-test=1\rfoo="\r"\rbar=\'\r\'\rbat=\r>\r</div\r>';
+  var doc = domino.createDocument(html);
+  var div = doc.querySelector('#a');
+  div.should.not.equal(null);
+  // all \r should be converted to \n
+  div.outerHTML.should.equal('<div id="a" data-test="1" foo="\n" bar="\n" bat="">\n</div>');
+};

commit cba17a67ca3dfedfebf95af2ea933e61ac7c42cd

Fix SIMPLETAG/SIMPLEATTR fast path.
This regression (caused by 400b989d96d3d0b11482808ab0cccbda98ee3e98)
caused some quoted attributes containing a space then an ampersand or
\r to be incorrectly parsed.  (Malformed) tags with a \r or \n immediately
after a < were also being incorrectly parsed.

Added test case to exercise this code path.

Closes #45.

lib/HTMLParser.js
@@ -1454,8 +1454,8 @@ var PLAINTEXT = /[^\r\u0000\uffff]*/g;
 // us from scanning past the lastIndex set. (Note that the desired matches
 // are always greater than 1 char long, so longest-match will ensure that .
 // is not matched unless the desired match fails.)
-var SIMPLETAG = /(?:(\/)?([a-z]+)>)|./g;
-var SIMPLEATTR = /(?:([-a-z]+) *= *('[^'&\r\u0000]*'|"[^"&\r\u0000]*"|[^&> \t\n\r\f\u0000]+[ \t\n\f]))|./g;
+var SIMPLETAG = /(?:(\/)?([a-z]+)>)|[\s\S]/g;
+var SIMPLEATTR = /(?:([-a-z]+)[ \t\n\f]*=[ \t\n\f]*('[^'&\r\u0000]*'|"[^"&\r\u0000]*"|[^\t\n\r\f "&'\u0000>][^&> \t\n\r\f\u0000]*[ \t\n\f]))|[\s\S]/g;
 
 var NONWS = /[^\x09\x0A\x0C\x0D\x20]/;
 var ALLNONWS = /[^\x09\x0A\x0C\x0D\x20]/g; // like above, with g flag

test/domino.js
@@ -333,4 +333,16 @@ exports.tagNameCase = function() {
   var qualifiedName = 'Envelope';
   var doc = impl.createDocument(namespace, qualifiedName, null);
   doc.documentElement.tagName.should.equal(qualifiedName);
-}
+};
+
+exports.fastAttributes = function() {
+  // test the SIMPLETAG/SIMPLEATTR path in HTMLParser
+  var html = "<div id=a b=\"x &quot;y\" c='a \rb'><\np></div>";
+  var doc = domino.createDocument(html);
+  var div = doc.querySelector('#a');
+  div.should.not.equal(null);
+  div.attributes.length.should.equal(3);
+  div.attributes.item(1).value.should.equal('x "y');
+  div.attributes.item(2).value.should.equal('a \nb');
+  div.children.length.should.equal(0);
+};
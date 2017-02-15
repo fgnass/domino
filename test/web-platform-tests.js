3d8e761d7db7be2e9eb23764915eb5abac572472
Pawel.Majewski <pmajewski@contour-technology.com>
2017-02-15 16:19:11 +0100

    enabling all platform tests

--- test/web-platform-tests.js
+++ test/web-platform-tests.js
@@ -328,6 +328,6 @@ var harness = function() {
 
 module.exports = harness(
   __dirname + '/web-platform-tests/custom-elements'
-  // __dirname + '/web-platform-tests/html/dom',
-  // __dirname + '/web-platform-tests/dom/nodes'
+  __dirname + '/web-platform-tests/html/dom',
+  __dirname + '/web-platform-tests/dom/nodes'
 );
